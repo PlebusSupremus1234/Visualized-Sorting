f6df7bbceff0b2d4aaf77b2027fe67163ba7db7c
PlebusSupremus1234 <hl13851@gmail.com>
2021-05-29 17:53:00 +1000

    Bubble Sort Mech

--- sketch.js
+++ sketch.js
@@ -1,7 +1,46 @@
+let array = [];
+let colW = 5;
+
+let currentBar;
+let i, j;
+
+let sort;
+
 function setup() {
-    createCanvas(800, 800);
+    createCanvas(1200, 800);
+    frameRate(60);
+    let amount = width / colW;
+    let spacing = (height - 80) / amount;
+    for (let i = 0; i < amount; i++) array.push(i * spacing);
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+
+    sort = "bubble";
 }
 
 function draw() {
     background(0);
+    for (let a = 0; a < array.length; a++) {
+        fill(a === currentBar ? "red" : 255);
+        rect(a * colW, height - array[a], colW, array[a]);
+    }
+    
+    for (let a = 0; a < 50; a++) {
+        if (sort === "bubble") {
+            if (i === undefined || j === undefined) {
+                i = array.length - 1;
+                j = 0;
+            }
+            if (j >= i) {
+                i--;
+                j = 0;
+            }
+            if (i < 0) noLoop();
+            if (array[j] > array[j + 1]) [array[j], array[j + 1]] = [array[j + 1], array[j]];
+            j++;
+            currentBar = j;
+        }
+    }
 }
--- sorts.js
+++ sorts.js
@@ -1,39 +1,24 @@
-function BubbleSort(array) {
-    let l = array.length;
-    for (let i = 0; i < l; i++) {
-        let biggest = 0;
-        for (let j = 0; j < array.length; j++) {
-            if (array[j] > biggest) biggest = array[j];
-            if (array[j] > array[j + 1]) [array[j], array[j + 1]] = [array[j + 1], array[j]];
-        }
-        array.splice(array.indexOf(biggest), 1);
-        array.push(biggest);
-        l--;
-    }
-    return array;
-}
-
-function SelectionSort(array) {     
+function* SelectionSort(array) {     
     for (let i = 0; i < array.length; i++) {
         let min = i;
         for (let j = i + 1; j < array.length; j++) {
             if (array[j] < array[min]) min = j; 
         }
         if (min != i) [array[i], array[min]] = [array[min], array[i]];
+        yield;
     }
-    return array;
 }
 
-function InsertionSort(array) {
+function* InsertionSort(array) {
     for (let i = 1; i < array.length; i++) {
         for (let j = 0; j < i; j++) {
             if (array[i] < array[j]) [array[i], array[j]] = [array[j], array[i]];
         }
+        yield;
     }
-    return array;
 }
 
-function MergeSort(array, half = array.length / 2) {
+function* MergeSort(array, half = array.length / 2) {
     function merger(left, right){
         const arr = [];  
         while (left.length && right.length){
@@ -43,15 +28,18 @@ function MergeSort(array, half = array.length / 2) {
         return [...arr, ...left, ...right];
     }
     if (array.length < 2) return array;  
-    const left = array.splice(0,half);  
+    const left = array.splice(0, half);
     return merger(MergeSort(left), MergeSort(array));
 }
 
-function QuickSort(array) {
+function* QuickSort(array) {
     if (array.length < 2) return array;  
     let p = array[0];    
     let l = []; 
     let r = [];  
-    for (let i = 1; i < array.length; i++) array[i] < p ? l.push(array[i]) : r.push(array[i]);  
+    for (let i = 1; i < array.length; i++) array[i] < p ? l.push(array[i]) : r.push(array[i]);
+    yield;
     return QuickSort(l).concat(p, QuickSort(r));
 }
+
+//fix merge and quick sort
