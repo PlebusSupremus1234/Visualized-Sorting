afaf4c8263e66360eafe0bd44fe6555e5d595a84
PlebusSupremus1234 <hl13851@gmail.com>
2021-05-30 10:29:57 +1000

    Play/Pause and Restart Button

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let array;
+let initial;
 let colW = 5;
 let amount;
 let spacing;
@@ -8,8 +9,11 @@ let i, j;
 
 let sort;
 
+let paused = true;
+
 function setup() {
-    createCanvas(1200, 800);
+    createCanvas(1200 + 450, 800);
+    width = 1200;
     amount = width / colW;
     spacing = (height - 80) / amount;
     frameRate(60);
@@ -19,21 +23,36 @@ function setup() {
 function draw() {
     background(0);
     
-    for (let a = 0; a < 200; a++) {
-        if (sort === "bubble") {
-            if (j >= i) {
-                i--;
-                j = 0;
+    if (!paused) {
+        for (let a = 0; a < 200; a++) {
+            if (sort === "bubble") {
+                if (j >= i) {
+                    i--;
+                    j = 0;
+                }
+                if (i < 0) noLoop();
+                if (array[j] > array[j + 1]) [array[j], array[j + 1]] = [array[j + 1], array[j]];
+                j++;
+                currentBar = j;
             }
-            if (i < 0) noLoop();
-            if (array[j] > array[j + 1]) [array[j], array[j + 1]] = [array[j + 1], array[j]];
-            j++;
-            currentBar = j;
         }
     }
 
+    stroke(0);
     for (let a = 0; a < array.length; a++) {
         fill(a === currentBar ? "red" : 255);
         rect(a * colW, height - array[a], colW, array[a]);
     }
+
+    fill("#ebebeb");
+    noStroke();
+    rect(width, 0, 450, height);
+
+    fill(0);
+    textSize(40);
+    textStyle(BOLD);
+    text("Visualized Sorting", width + 15, 50);
+    textSize(30);
+    text("Play or Pause:", width + 15, 90);
+    text("Restart:", width + 15, 130);
 }
--- util.js
+++ util.js
@@ -1,14 +1,37 @@
-function init(type) {
-    array = [];
-    for (let a = 0; a < amount; a++) array.push(a * spacing);
-    for (let a = array.length - 1; a > 0; a--) {
-        let b = Math.floor(Math.random() * (a + 1));
-        [array[a], array[b]] = [array[b], array[a]];
-    }
+function init(type, same) {
+    if (!paused) playpause();
+    if (!same) {
+        array = [];
+        for (let a = 0; a < amount; a++) array.push(a * spacing);
+        for (let a = array.length - 1; a > 0; a--) {
+            let b = Math.floor(Math.random() * (a + 1));
+            [array[a], array[b]] = [array[b], array[a]];
+        }
+
+        initial = [].concat(array); //concat for duplicating array,
+    } else array = [].concat(initial); //so it isnt a pointer
 
     sort = type;
     if (type === "bubble") {
         i = array.length - 1;
         j = 0;
+    }
+
+    currentBar = null;
+}
+
+function playpause() {
+    let element = document.getElementById("playpause");
+    element.classList.toggle("paused");
+    paused = !paused;
+}
+
+function restart() {
+    let element = document.getElementById("restart");
+    if (!element.classList.contains("clicked")) {
+        element.classList.toggle("clicked");
+        setTimeout(() => element.classList.toggle("clicked"), 800);
+
+        init(sort, true);
     }
 }
